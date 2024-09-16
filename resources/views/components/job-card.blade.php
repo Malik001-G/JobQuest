f8c3a7c99e851332a51e533ddfc022cdff8243c7
Malik001-G <adisamalikng0@gmail.com>
2024-09-16 04:20:36 +0100

    Jobs card component

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,21 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <a href="#"
+                class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-all duration-500 ease-in-out">Tag</a>
+            <a href="#"
+                class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-all duration-500 ease-in-out">Tag</a>
+            <a href="#"
+                class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-all duration-500 ease-in-out">Tag</a>
+
+        </div>
+        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+    </div>
+
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,9 @@
 <x-layout>
     <section>
-        <h3 class="mt-6">Top Jobs</h3>
+        <h3 class="mb-6">Top Jobs</h3>
 
-        <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
-
-            <div class="self-start text-sm">Laracasts</div>
-            <div class="py-8 font-bold">
-                <h3>Video Producer</h3>
-                <p>Full time - From $60,000</p>
-            </div>
-            <div class="flex justify-between items-center mt-auto">
-                <div>
-                    <a href="#">Tag</a>
-                    <a href="#">Tag</a>
-                    <a href="#">Tag</a>
-                </div>
-                <img src="http://placehold.it/42/42" alt="">
-            </div>
-
-        </div>
+        <x-job-card />
+        <x-job-card />
+        <x-job-card />
     </section>
 </x-layout>
