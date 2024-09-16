8c564854898cf9c57ac35f42f69221434cc7f3ca
Malik001-G <adisamalikng0@gmail.com>
2024-09-16 04:14:27 +0100

    Setting the layout- nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,23 @@
 <x-layout>
-    Hello
+    <section>
+        <h3 class="mt-6">Top Jobs</h3>
+
+        <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
+            <div class="self-start text-sm">Laracasts</div>
+            <div class="py-8 font-bold">
+                <h3>Video Producer</h3>
+                <p>Full time - From $60,000</p>
+            </div>
+            <div class="flex justify-between items-center mt-auto">
+                <div>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                </div>
+                <img src="http://placehold.it/42/42" alt="">
+            </div>
+
+        </div>
+    </section>
 </x-layout>
